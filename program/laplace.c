1ce71cd872d8e6c0691e911b40eb8f2d90a186b8
XMP Specification Group <dummy@dummy.jp>
2010-02-14 17:44:56 +0000

    first commit

---
+++ program/laplace.c
@@ -0,0 +1,39 @@
+/*
+ *  A parallel explicit solver of Laplace equation in \XMP
+ */
+#pragma xmp nodes p(NPROCS)
+#pragma xmp template t(1:N)
+#pragma xmp distribute t(block) on p
+
+double u[XSIZE+2][YSIZE+2],
+       uu[XSIZE+2][YSIZE+2];
+#pragma xmp align u[i][*] to t(i)
+#pragma xmp align uu[i][*] to t(i)
+#pragma xmp shadow uu[1:1][0:0]
+
+lap_main()
+{ 
+ int x,y,k;
+ double sum;
+for(k = 0; k < NITER; k++){
+	/* old <- new */
+#pragma xmp loop on t(x)
+	for(x = 1; x <= XSIZE; x++)
+	  for(y = 1; y <= YSIZE; y++)
+	    uu[x][y] = u[x][y];
+#pragma xmp reflect uu
+#pragma xmp loop on t(x)
+	for(x = 1; x <= XSIZE; x++)
+	  for(y = 1; y <= YSIZE; y++)
+	    u[x][y] = (uu[x-1][y] + uu[x+1][y] +
+                  uu[x][y-1] + uu[x][y+1])/4.0;
+    }
+m */
+    sum = 0.0;
+#pragma xmp loop on t[x] reduction(+:sum)
+    for(x = 1; x <= XSIZE; x++)
+	for(y = 1; y <= YSIZE; y++)
+	  sum += (uu[x][y]-u[x][y]);
+#pragma xmp task on master
+    printf("sum = %g\n",sum);
+}
---
+++ program/linpack.c
@@ -0,0 +1,140 @@
+/*
+ * Linpack in XcalableMP (Gaussian elimination with partial pivoting) 
+ *    1D distribution version 
+ */
+#pragma xmp nodes p(*)
+#pragma xmp template t(0:LDA-1)
+#pragma xmp distribute (cyclic): t
+
+double pvt_v[N];  // local
+
+/*     gaussian elimination with partial pivoting	*/
+dgefa(double a[n][LDA],int lda, int n,int ipvt,int *info)
+#pragma xmp align a[:][i] to t(i)
+{
+    REAL t;
+    int idamax(),j,k,kp1,l,nm1,i;
+    REAL x_pvt;
+
+    nm1 = n - 1;
+    for (k = 0; k < nm1; k++) {
+      kp1 = k + 1;
+      /* find l = pivot index	*/
+      l = A_idamax(k,n-k,a[k]);
+      ipvt[k] = l;
+
+      /* if (a[k][l] != ZERO) */
+#ifdef XMP
+#pragma xmp gmove
+      pvt_v[k:n-1] = a[l][k:n-1];
+#else
+      for(i = k; i < n; i++) pvt_v[i] = a[i][l];
+#endif
+
+      /* interchange if necessary */
+      if (l != k){
+#ifdef XMP
+#pragm xmp gmove
+	a[l][:] = a[k][:];
+#pramga xmp gmove
+	a[k][:] = pvt_v[:];
+#else
+	for(i = k; i< n; i++) a[i][l] = a[i][k];
+	for(i = k; i< n; i++) a[i][k] = pvt_v[i];
+#endif
+      }
+      /* compute multipliers */
+      t = -ONE/pvt_v[k];
+      A_dscal(k+1, n-(k+1),t,a[k]);
+      
+      /* row elimination with column indexing */
+      for (j = kp1; j < n; j++) {
+	t = pvt_v[j];
+	A_daxpy(k+1,n-(k+1),t,a[k],a[j]);
+      } 
+    }
+    ipvt[n-1] = n-1;
+}
+
+dgesl(double a[n][LDA],int lda,int n,int pvt[n],double b,int job)
+#pragma xmp align a[:][i] to t(i)
+#pragma xmp align b[i] to t(i)
+{
+    REAL t;
+    int k,kb,l,nm1;
+    
+    nm1 = n - 1;
+    /* job = 0 , solve  a * x = b,  first solve  l*y = b  */
+    for (k = 0; k < nm1; k++) {
+	l = ipvt[k];
+#pragma xmp gmove
+	t = b[l];
+	if (l != k){ 
+#pragma xmp gmove
+	    b[l] = b[k];
+#pragma xmp gmove
+	    b[k] = t;
+	}	
+	A_daxpy(k+1,n-(k+1),t,a[k],b);
+    }
+
+    /* now solve u*x = y */
+    for (kb = 0; kb < n; kb++) {
+	k = n - (kb + 1);
+#pragma xmp task on t(k)
+{
+	b[k] = b[k]/a[k][k];
+	t = -b[k];
+}
+#pragma xmp bcast t from t(k)
+	A_daxpy(0,k,t,a[k],b);
+    }
+}
+
+/* 
+* distributed array based routine 
+*/
+A_daxpy(int b,int n,double da,double dx[n],double dy[n])
+#pragma xmp align dx[i], dy[i] to t(i)
+{
+    int i,ix,iy,m,mp1;
+    if(n <= 0) return;
+    if(da == ZERO) return;
+    /* code for both increments equal to 1 */
+#pragma xmp loop on t(b+i)
+    for (i = 0;i < n; i++) {
+	dy[b+i] = dy[b+i] + da*dx[b+i];
+    }
+}
+
+int A_idamax(int b,int n,double dx[n])
+#pragma xmp align dx[i] to t(i)
+{
+  double dmax, g_dmax;
+    int i, ix, itemp;
+    if(n == 1) return(0);
+
+    /* code for increment equal to 1 */
+    itemp = 0;
+    dmax = 0.0;
+#pragma xmp loop on t(i) reduction(lastmax:dmax/itemp/)
+    for (i = b; i < n; i++) {
+      if(fabs((double)dx[i]) > dmax) {
+	itemp = i;
+	dmax = fabs((double)dx[i]);
+      }
+    }
+    return (itemp);
+}
+
+A_dscal(int b,int n,double da,double dx[n])
+#pragma xmp align dx[i], dy[i] to t(i)
+{
+    int i;
+    if(n <= 0)return;
+
+    /* code for increment equal to 1 */
+#pragma xmp loop on t(i)
+    for (i = b; i < n; i++)
+      dx[i] = da*dx[i];
+}
